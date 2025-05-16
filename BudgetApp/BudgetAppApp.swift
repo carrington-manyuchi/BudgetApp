ec8a2ec86233093cbbf57739131d9cb4109705aa
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-16 15:03:42 +0200

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Manyuchi, Carrington C on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
